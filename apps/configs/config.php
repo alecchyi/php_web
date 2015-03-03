f340f71bed41f18a02ae45466351cb80ed7a696a
alecchyi <alecchyi@gmail.com>
2015-03-04 03:52:47 +0800

    update locale

--- apps/configs/config.php
+++ apps/configs/config.php
@@ -3,17 +3,21 @@
 
 date_default_timezone_set('GMT'); 
 
+session_start();
+
 $lang = 'en_US.UTF-8'; // Dummy
-setlocale(LC_ALL, $lang); 
+setlocale(LC_ALL, $lang);
 
 $locale = "zh_CN";
+if(isset($_SESSION['locale'])){
+    $locale = $_SESSION['locale'];
+}
 putenv("LANGUAGE=$locale");
 setlocale(LC_ALL, $locale);
 bindtextdomain("messages", "./locale");
+bind_textdomain_codeset("messages","UTF-8");
 textdomain("messages");
 
-
-echo _("nav_home");
 /*
  * Smarty config
  */
@@ -34,8 +38,8 @@ $smarty -> assign("filename",$FILENAME);
 $DOC = array(
     "title" => "title",
     "meta" => array(
-        "keywords" => "keywords",
-        "description" => "description"
+	"keywords" => "keywords",
+	"description" => "description"
     )
 );
 
