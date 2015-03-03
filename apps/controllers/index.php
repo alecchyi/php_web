862e0b7453b57dd38712eb1b7a44dd93c0b845db
alecchyi <alecchyi@gmail.com>
2015-03-03 15:19:18 +0800

    init project
    init project

--- apps/controllers/index.php
+++ apps/controllers/index.php
@@ -1,8 +1,5 @@
 <?php
 
-$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-ini_set('include_path',$root); 
-
 require_once($root . "/apps/configs/config.php");
 require_once($root . "/apps/models/product.php");
 
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+ini_set('include_path',$root); 
+
 require_once("apps/controllers/index.php");
 
 ?>
